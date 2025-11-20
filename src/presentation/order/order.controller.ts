2d68f8077a9273ea30f0807cf11255f805172e27
Yalda Mahmoodi <yaldamahmoodi10@gmail.com>
2025-11-20 23:06:30 +0330

    feat: cancelOrder and executeOrder method added in OrderContoller class

--- src/presentation/order/order.controller.ts
+++ src/presentation/order/order.controller.ts
@@ -1,15 +1,48 @@
 import {Request, Response} from "express";
 import {CreateOrder} from "../../application/order/create-order.usecase";
 import {createOrderSchema} from "./dto/create-order.dto";
+import {ActionOrderDto, actionOrderSchema} from "./dto/action-order.dto";
+import {ExecuteOrder} from "../../application/order/execute-order.usecase";
+import {handleError} from "../shared/handle-error"
+import {CancelOrder} from "../../application/order/cancel-order.usecase";
 
 export class OrderController {
-    constructor(private readonly orderUseCase: CreateOrder) {
-        this.create = this.create.bind(this);
+    constructor(private readonly createOrderUseCase: CreateOrder,
+                private readonly executeOrderUseCase: ExecuteOrder,
+                private readonly cancelOrderUseCase: CancelOrder,) {
+        this.createOrder = this.createOrder.bind(this);
+        this.executeOrder = this.executeOrder.bind(this);
+        this.cancelOrder = this.cancelOrder.bind(this);
     }
 
-    async create(req: Request, res: Response) {
-        const dto = createOrderSchema.parse(req.body);
-        const result = await this.orderUseCase.execute(dto);
-        return res.status(201).json(result);
+    async createOrder(req: Request, res: Response) {
+        try {
+            const dto = createOrderSchema.parse(req.body);
+            const result = await this.createOrderUseCase.execute(dto);
+            return res.status(201).json(result);
+        } catch (err: any) {
+            return handleError(err, res);
+        }
     }
+
+    async executeOrder(req: Request, res: Response) {
+        try {
+            const dto: ActionOrderDto = actionOrderSchema.parse(req.body);
+            await this.executeOrderUseCase.execute(dto.id);
+            return res.status(200).json({message: "Order executed successfully"});
+        } catch (err: any) {
+            return handleError(err, res);
+        }
+    }
+
+    async cancelOrder(req: Request, res: Response) {
+        try {
+            const dto: ActionOrderDto = actionOrderSchema.parse(req.body);
+            await this.cancelOrderUseCase.execute(dto.id);
+            return res.status(200).json({message: "Order canceled successfully"});
+        } catch (err: any) {
+            return handleError(err, res);
+        }
+    }
+
 }
