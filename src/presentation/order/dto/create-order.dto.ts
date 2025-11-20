8665ff1804fa4378352e97ef9480c2daa07a810b
Yalda Mahmoodi <yaldamahmoodi10@gmail.com>
2025-11-20 23:04:04 +0330

    fix: update Zod validation to get Error message

--- src/presentation/order/dto/create-order.dto.ts
+++ src/presentation/order/dto/create-order.dto.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const createOrderSchema = z.object({
-    userId: z.string(),
-    originToken: z.string(),
-    destinationToken: z.string(),
-    amount: z.number(),
+    userId: z.string().min(1, "userId is required"),
+    originToken: z.string().min(1, "originToken is required"),
+    destinationToken: z.string().min(1, "destinationToken is required"),
+    amount: z.coerce.number().refine(val => val > 0, { message: "amount must be greater than 0" }),
 });
 
 export type CreateOrderDTO = z.infer<typeof createOrderSchema>;
